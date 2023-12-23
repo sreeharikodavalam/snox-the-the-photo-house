b845049f34e049aa6e8ac80b0e0c983bd9ed3a4a
sreeharikodavalam <sreeharikodavalam@gmail.com>
2023-12-23 11:51:17 +0530

    Face detection and whatsapp sharing initial setup No task and scheduled jobs, "Need to work on this"

--- authentication/models.py
+++ authentication/models.py
@@ -5,14 +5,20 @@ from django.contrib.auth.models import User
 class UserDesignation(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = 'auth_user_designations'
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='user/profile_pics/', null=True, blank=True)
-    designation = models.OneToOneField(UserDesignation, on_delete=models.CASCADE)
+    designation = models.ForeignKey(UserDesignation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
     class Meta:
         db_table = 'auth_user_profile'
